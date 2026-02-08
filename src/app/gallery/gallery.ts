59be6fc9595141e8dbe81dfbb36b223ca5463f6e
DiljotSingh2005 <diljotrnp@gmail.com>
2026-02-08 11:15:37 +0530

    added gallery page

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
 export class Gallery {
+      categories = [
+    'All',
+    'Science Fair',
+    'Annual Day',
+    'Classrooms',
+    'Drawing Competition',
+    'Sports Day',
+    'Independence Day'
+  ];
+
+  // 🔥 IMPORTANT: activeCategory define hona chahiye
+  activeCategory: string = 'All';
+
+  images = [
+    { src: '/g1.jpeg', category: 'Science Fair' },
+    { src: '/g2.jpeg', category: 'Science Fair' },
+    { src: '/g3.jpeg', category: 'Annual Day' },
+    { src: '/g4.jpeg', category: 'Classrooms' },
+    { src: '/g5.jpeg', category: 'Sports Day' },
+    { src: '/g6.jpeg', category: 'Drawing Competition' },
+    { src: '/g7.jpeg', category: 'Independence Day' },
+    { src: '/g8.jpeg', category: 'Annual Day' }
+  ];
+
+  // 🔥 IMPORTANT: setCategory function hona chahiye
+  setCategory(category: string) {
+    this.activeCategory = category;
+  }
+
+  filteredImages() {
+    if (this.activeCategory === 'All') {
+      return this.images;
+    }
+    return this.images.filter(
+      img => img.category === this.activeCategory
+    );
+  }
 
 }
