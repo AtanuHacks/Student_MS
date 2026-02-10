9c0cb05b9a79625fe6b9e554f003a1315497f33d
Aryan0508-code <aryankeshri0508@gmail.com>
2026-02-10 18:42:11 +0530

    Added Prospectus , modified ContactUs

--- src/app/prospectus/prospectus.ts
+++ src/app/prospectus/prospectus.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-prospectus',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,NgxExtendedPdfViewerModule],
   templateUrl: './prospectus.html',
   styleUrl: './prospectus.css',
 })
 export class Prospectus {
 
+  next_step = 0;
+
+next() {
+  this.next_step = 1;
+}
+
+closeViewer() {
+  this.next_step = 0;
+}
 }
